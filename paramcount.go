3aa9f6347803efd94d882b28360433ea72cd0dd5
hiombima <ombimahillary52@gmail.com>
2024-05-22 16:57:59 +0300

    initial update

--- paramcount.go
+++ paramcount.go
@@ -2,18 +2,56 @@ package main
 
 import (
 	"os"
-)
 
-func paramcount(s []string) int {
-	count := len(s)
-	return count
-}
+	"github.com/01-edu/z01"
+)
 
 func main() {
 	if len(os.Args) <= 1 {
-		println(0)
+		z01.PrintRune('0')
+		z01.PrintRune('\n')
 		return
 	}
-	args := os.Args[1:]
-	println(paramcount(args))
+	arg := os.Args[1:]
+	count := '0'
+	for range arg {
+		count++
+	}
+
+	z01.PrintRune(count)
+	z01.PrintRune('\n')
+
 }
+
+// func ParamCount()int  {
+	// var count int
+	// word := os.Args[1:]
+	// for range word {
+	// 	count++
+	// }
+	// return count
+// }
+
+// func PrintLn(num int) {
+// 	for _, ch := range Itoa(num) {
+// 		z01.PrintRune(ch)
+// 	}
+// 	z01.PrintRune('\n')
+// }
+
+// func Itoa(n int) string {
+// 	if n == 0 {
+// 		z01.PrintRune('0')
+// 	}
+// 	if n < 0 {
+// 		z01.PrintRune('-')
+// 		n = -n
+// 	}
+// 	var digits []rune
+// 	for n > 0 {
+// 		digit := n % 10
+// 		digits = append([]rune{rune('0' + digit)}, digits...)
+// 		n /= 10
+// 	}
+// 	return string(digits)
+// }
