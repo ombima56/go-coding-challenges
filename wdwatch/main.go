0839bf90251ef44ab9279836ad02fcf80c882872
hiombima <ombimahillary52@gmail.com>
2024-05-23 11:44:52 +0300

    updates

--- wdwatch/main.go
+++ wdwatch/main.go
@@ -16,17 +16,22 @@ func main() {
 	pos1 := 0
 	pos2 := 0
 
-	for pos2 < len(args2) {
-		if pos1 < len(args1) && args1[pos1] == args2[pos2] {
+	// for pos2 < len(args2) {
+	// 	if pos1 < len(args1) && args1[pos1] == args2[pos2] {
+	// 		pos1++
+	// 	}
+	// 	pos2++
+	for pos1 < len(args1) && pos2 < len(args2) {
+		if args1[pos1] == args2[pos2] {
 			pos1++
 		}
 		pos2++
+	}
 
-		if pos2 == len(args1) {
-			for _, ch := range args1 {
-				z01.PrintRune(ch)
-			}
-			z01.PrintRune('\n')
+	if pos1 == len(args1) {
+		for _, ch := range args1 {
+			z01.PrintRune(ch)
 		}
+		z01.PrintRune('\n')
 	}
 }
