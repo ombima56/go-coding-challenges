c42af9d92ae1b1821a176ffb10ba02c65df2d6d7
ombima56 <ombimahillary6@gmail.com>
2024-07-21 14:52:00 +0300

    Add displayfirstparam

---
+++ displayfirstparam/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	if len(os.Args) <= 1 {
+		return
+	}
+	words := os.Args[1:]
+	for _, word := range words[0] {
+		z01.PrintRune(word)
+	}
+	z01.PrintRune('\n')
+}
