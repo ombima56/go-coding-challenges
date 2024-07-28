b10767b11743702ece141ac6ffbb2a1d2d5d4f2f
konditi1 <120721623+konditi1@users.noreply.github.com>
2024-07-28 11:19:53 +0300

    Update main.go
    changed the implementation and added test cases
    just run it  like this
    go run main.go

--- canjump/main.go
+++ canjump/main.go
@@ -2,10 +2,47 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"reflect"
 )
 
+func main() {
+	tests := []struct {
+		args []uint
+		want bool
+	}{
+		{args: []uint{2, 3, 1, 1, 4}, want: true},
+		{args: []uint{1, 1, 1, 1, 0}, want: true},
+		{args: []uint{5, 4, 3, 2, 1, 0}, want: true},
+		{args: []uint{0}, want: true},
+		{args: []uint{5}, want: true},
+		{args: []uint{}, want: false},
+		{args: []uint{1, 2, 3, 0, 2}, want: false},
+		{args: []uint{3, 2, 1, 0, 4}, want: false},
+		{args: []uint{0, 0, 0, 0, 0}, want: false},
+		{args: []uint{1, 2, 3}, want: false},
+		{args: []uint{1, 2, 3, 0, 1}, want: false},
+		{args: []uint{1, 0, 0, 0, 0}, want: false},
+		{args: []uint{1, 0, 1, 0, 1}, want: false},
+		{args: []uint{10, 20, 30, 40, 0}, want: false},
+	}
+
+	for _, tc := range tests {
+		got := CanJump(tc.args)
+		if !reflect.DeepEqual(got, tc.want) {
+			log.Fatalf("%s(%+v) == %+v instead of %+v\n",
+				"CanJump",
+				tc.args,
+				got,
+				tc.want,
+			)
+		}
+	}
+	fmt.Println("all test passed")
+}
+
 func CanJump(slice []uint) bool {
-	n := len(slice)
+
 	if len(slice) == 0 {
 		return false
 	}
@@ -14,31 +51,18 @@ func CanJump(slice []uint) bool {
 		return true
 	}
 
-	var reachable int
+	var steps int
 	for i := 0; i < len(slice); i++ {
-		// If the current index is beyond the furthest reachable index
-		if i > reachable {
+		//if you encounter 0 in the slice you wont move so just return false
+		if i > steps {
 			return false
 		}
-		if int(i)+(int(slice[i])) > reachable {
-			reachable = int(i) + int(slice[i])
-		}
-		// If reachable index is beyond or at the last index
-		if reachable >= n-1 {
+		if steps == len(slice)-1 {
 			return true
+		} else {
+			steps = i + int(slice[i])
 		}
 	}
 	// If we finish the loop and haven't reached the last index, return false
 	return false
-}
-
-func main() {
-	input1 := []uint{2, 3, 1, 1, 4}
-	fmt.Println(CanJump(input1))
-
-	input2 := []uint{3, 2, 1, 0, 4}
-	fmt.Println(CanJump(input2))
-
-	input3 := []uint{0}
-	fmt.Println(CanJump(input3))
 }
