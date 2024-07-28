1b8d5313ac6638312ec218b963cc421b2022ab64
konditi1 <120721623+konditi1@users.noreply.github.com>
2024-07-28 18:12:37 +0300

    Update main.go
    test: add test cases

--- hashcode/main.go
+++ hashcode/main.go
@@ -1,25 +1,38 @@
 package main
 
-import "fmt"
-
-func HashCode(dec string) string {
-	var result string
-	size := len(dec)
-
-	for _, char := range dec {
-		hashedChar := (int(char) + size) % 127
-		if hashedChar < 32 || hashedChar > 126 {
-			hashedChar += 33
-		}
-		result += string(rune(hashedChar))
-	}
-
-	return result
-}
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println(HashCode("A"))
-	fmt.Println(HashCode("AB"))
-	fmt.Println(HashCode("BAC"))
-	fmt.Println(HashCode("Hello World"))
+	table := []string{"Z", "Hi!", "BB198365", "sabito", "14 Avril 1912", "zyx987bca", "		pool-2020", "965truma747", " Mercedes-AMG GT"}
+	for _, s := range table {
+		fmt.Println(HashCode(s), solutions(s))
+	}
+}
+
+func HashCode(dec string) (str string) {
+	n := len(dec)
+
+	for _, v := range dec {
+		v = (v + rune(n)) % 127
+		if v < 32 {
+			v += 33
+		}
+		str += string(v)
+	}
+	return
+}
+
+func solutions(dec string) string {
+	size := len(dec)
+	hashed := ""
+	for _, char := range dec {
+		hash := (int(char) + size) % 127
+		if hash < 32 || hash > 126 {
+			hash += 33
+		}
+		hashed += string(hash)
+	}
+	return hashed
 }
