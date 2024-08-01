123b9eefcf8d613cf16e98c0ce05f14382d78f28
ombima56 <ombimahillary6@gmail.com>
2024-08-02 01:29:30 +0300

    feat: add check for checking the len of the string

--- zipstring/main.go
+++ zipstring/main.go
@@ -6,18 +6,19 @@ import (
 )
 
 func ZipString(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
 	var zip string
 	var count = 1
-
 	for i := 1; i < len(s); i++ {
 		if s[i] == s[i-1] {
 			count++
 		} else {
-			zip += strconv.Itoa(count) + (string(s[i-1]))
+			zip += strconv.Itoa(count) + string(s[i-1])
 			count = 1
 		}
 	}
-	// Append the last counted charracters
 	zip += strconv.Itoa(count) + string(s[len(s)-1])
 	return zip
 }
