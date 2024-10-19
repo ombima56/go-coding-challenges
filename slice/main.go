c1889ea63fa8339c4eb0ac54db14a8f45f6623fd
ombima56 <ombimahillary6@gmail.com>
2024-10-20 02:46:05 +0300

    refactor: refactor the function maintaining it's functionality

--- slice/main.go
+++ slice/main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	a := []string{"coding", "algorithm", "ascii", "package", "golang"}
@@ -14,48 +12,36 @@ func main() {
 }
 
 func Slice(a []string, nbrs ...int) []string {
-	if len(nbrs) == 0 {
-		return a
+	start := 0
+	end := len(a)
+
+	if len(nbrs) > 0 {
+		start = nbrs[0]
+		if start > len(a) {
+			start = len(a)
+		}
+		if start < 0 {
+			start = start + len(a)
+		}
+		if start < 0 {
+			start = 0
+		}
 	}
 
-	start := nbrs[0]
-	if start < 0 {
-		start += len(a)
-	}
-
-	if len(nbrs) == 1 {
-		if start < 0 || start >= len(a) {
+	if len(nbrs) > 1 {
+		end = nbrs[1]
+		if end > len(a) {
+			end = len(a)
+		}
+		if end < 0 {
+			end = end + len(a)
+		}
+		if end < 0 {
+			end = 0
+		}
+		if start > end {
 			return nil
 		}
-		return a[start:]
-	}
-
-	end := nbrs[1]
-	if end < 0 {
-		end += len(a)
-	}
-
-	// Special case: if end is 0, return nil
-	if end == 0 {
-		return nil
-	}
-
-	// Return empty slice if both indices are out of bounds but in correct order
-	if start < 0 && end <= 0 && start < end {
-		return []string{}
-	}
-
-	// Ensure start and end are within bounds
-	if start < 0 {
-		start = 0
-	}
-	if end > len(a) {
-		end = len(a)
-	}
-
-	// Return nil if the range is invalid
-	if start >= end || start >= len(a) {
-		return nil
 	}
 
 	return a[start:end]
