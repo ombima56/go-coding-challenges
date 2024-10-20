3ad43bba4bbeef7a3621f37ddbdaa41aa8b31120
ombima56 <ombimahillary6@gmail.com>
2024-10-20 03:49:03 +0300

    refactor: optimize ExpandStr function for better performance

--- expandstr/main.go
+++ expandstr/main.go
@@ -12,33 +12,33 @@ func main() {
 	}
 
 	args := os.Args[1]
-	var word []rune
-	var result []rune
-	inWord := false
-
-	for _, ch := range args {
-		if ch != ' ' {
-			word = append(word, ch)
-			inWord = true
-		} else if inWord {
-			if len(result) > 0 {
-				result = append(result, ' ', ' ', ' ')
-			}
-			result = append(result, word...)
-			word = nil
-			inWord = false
-		}
-	}
-
-	if inWord {
-		if len(result) > 0 {
-			result = append(result, ' ', ' ', ' ')
-		}
-		result = append(result, word...)
-	}
+	result := ExandStr(args)
 
 	for _, ch := range result {
 		z01.PrintRune(ch)
 	}
 	z01.PrintRune('\n')
+}
+
+func ExandStr(s string) string {
+	spaceNeeded := false // Tracks if we need to add a space before a new word
+	wordStarted := false // Tracks if we're inside a word
+	// var result string
+	var result []rune
+
+	for _, ch := range s {
+		if ch != ' ' {
+			if spaceNeeded {
+				result = append(result, ' ', ' ', ' ')
+				// result += "   "
+			}
+			// result += string(ch)
+			result = append(result, ch)
+			wordStarted = true
+			spaceNeeded = false
+		} else if wordStarted {
+			spaceNeeded = true // Set flag to insert space before the next word
+		}
+	}
+	return string(result)
 }
