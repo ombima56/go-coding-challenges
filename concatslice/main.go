1eae85156832086a58644682f1cb5446acf7654b
ombima56 <ombimahillary6@gmail.com>
2024-10-20 04:23:11 +0300

    chore: remove redundant lines

--- concatslice/main.go
+++ concatslice/main.go
@@ -1,33 +1,11 @@
 package main
 
-// import "fmt"
-
-// func main() {
-// 	fmt.Println(ConcatSlice([]int{1, 2, 3}, []int{4, 5, 6}))
-// 	fmt.Println(ConcatSlice([]int{}, []int{4, 5, 6, 7, 8, 9}))
-// 	fmt.Println(ConcatSlice([]int{1, 2, 3}, []int{}))
-// }
-
-import (
-	// student "student"
-
-	"github.com/01-edu/go-tests/lib/challenge"
-	"github.com/01-edu/go-tests/solutions"
-)
+import "fmt"
 
 func main() {
-	args := [][][]int{
-		{{1, 2, 3}, {4, 5, 6}},
-		{{}, {-10, 0, 2}},
-		{{-10, 0, 2}, {}},
-		{{}, {}},
-		{{1, 2, 3}, {4, 5, 6, 3, 4, 5, 6}},
-		{{0, 0, 0}, {0, 0, 0}},
-	}
-
-	for _, arg := range args {
-		challenge.Function("ConcatSlice", ConcatSlice, solutions.ConcatSlice, arg[0], arg[1])
-	}
+	fmt.Println(ConcatSlice([]int{1, 2, 3}, []int{4, 5, 6}))
+	fmt.Println(ConcatSlice([]int{}, []int{4, 5, 6, 7, 8, 9}))
+	fmt.Println(ConcatSlice([]int{1, 2, 3}, []int{}))
 }
 
 func ConcatSlice(slice1, slice2 []int) []int {
