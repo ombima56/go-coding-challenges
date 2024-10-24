ad609ca2f5d269aad44320ce718291a3009ada1c
ombima56 <ombimahillary6@gmail.com>
2024-10-24 23:37:10 +0300

    refactor: optimize sorting algorithm to O(n log n), maintaining functionality

--- printmemory/printmemorywith01/main.go
+++ printmemory/printmemorywith01/main.go
@@ -1,26 +1,48 @@
 package main
 
+// import (
+// 	"github.com/01-edu/z01"
+// )
+
+// func main() {
+// 	PrintMemory([10]byte{'h', 'e', 'l', 'l', 'o', 16, 21, '*'})
+// }
+
 import (
+	"github.com/01-edu/go-tests/lib/challenge"
+	"github.com/01-edu/go-tests/lib/random"
+	"github.com/01-edu/go-tests/solutions"
 	"github.com/01-edu/z01"
 )
 
 func main() {
-	PrintMemory([10]byte{'h', 'e', 'l', 'l', 'o', 16, 21, '*'})
+	table := [10]byte{}
+
+	for j := 0; j < 5; j++ {
+		for i := 0; i < random.IntBetween(7, 10); i++ {
+			table[i] = byte(random.IntBetween(13, 126))
+		}
+		challenge.Function("PrintMemory", PrintMemory, solutions.PrintMemory, table)
+	}
+	table2 := [10]byte{'h', 'e', 'l', 'l', 'o', 16, 21, '*'}
+	challenge.Function("PrintMemory", PrintMemory, solutions.PrintMemory, table2)
 }
 
 func PrintMemory(arr [10]byte) {
-	for i, v := range arr {
-		printStr(Hex(int(v)))
+	for i, ch := range arr {
+		printHex(ch)
 		if (i+1)%4 == 0 {
 			z01.PrintRune('\n')
 		} else if i != len(arr)-1 {
 			z01.PrintRune(' ')
 		}
 	}
-	z01.PrintRune('\n')
+	if len(arr)-1 != 0 {
+		z01.PrintRune('\n')
+	}
 
 	for _, ch := range arr {
-		if ch >= 32 && ch <= 126 {
+		if ch > 32 && ch <= 126 {
 			z01.PrintRune(rune(ch))
 		} else {
 			z01.PrintRune('.')
@@ -29,27 +51,8 @@ func PrintMemory(arr [10]byte) {
 	z01.PrintRune('\n')
 }
 
-func Hex(num int) string {
-	var res string
+func printHex(num byte) {
 	hex := "0123456789abcdef"
-
-	if num == 0 {
-		return "00"
-	}
-	for num > 0 {
-		digit := num % 16
-		res = string(hex[digit]) + res
-		num /= 16
-	}
-
-	if len(res) == 1 {
-		res = "0" + res
-	}
-	return res
-}
-
-func printStr(s string) {
-	for _, ch := range s {
-		z01.PrintRune(ch)
-	}
+	z01.PrintRune(rune(hex[num/16])) // Finding the most significant digit
+	z01.PrintRune(rune(hex[num%16])) // finding the least significant digit
 }
