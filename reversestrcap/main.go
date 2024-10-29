689ab00d399f239638b32ec8acbd1e0ddd7d2f5e
ombima56 <ombimahillary6@gmail.com>
2024-10-30 02:22:44 +0300

    chore: remove redudant line

--- reversestrcap/main.go
+++ reversestrcap/main.go
@@ -3,9 +3,6 @@ package main
 import (
 	"os"
 
-	"github.com/01-edu/go-tests/lib/challenge"
-	"github.com/01-edu/go-tests/lib/chars"
-	"github.com/01-edu/go-tests/lib/random"
 	"github.com/01-edu/z01"
 )
 
@@ -23,23 +20,6 @@ func main() {
 		}
 	}
 	z01.PrintRune('\n')
-
-	{
-		args := [][]string{
-			{"First SMALL TesT"},
-			{"SEconD Test IS a LItTLE EasIEr", "bEwaRe IT'S NoT HARd WhEN ", " Go a dernier 0123456789 for the road e"},
-			{""},
-		}
-
-		for i := 0; i < 15; i++ {
-			args = append(args, random.StrSlice(chars.Alnum))
-		}
-
-		for _, v := range args {
-			challenge.Program("reversestrcap", v...)
-		}
-		challenge.Program("reversestrcap")
-	}
 }
 
 func formatWords(s string) string {
