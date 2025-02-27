fe8dffd7ff80b6ab564c74e7348e4031945351f0
ombima56 <ombimahillary6@gmail.com>
2025-02-27 23:50:29 +0300

    fix: ensure non-printable characters do not include spaces in printable values

--- printmemory/printmemorywith01/main.go
+++ printmemory/printmemorywith01/main.go
@@ -32,7 +32,7 @@ func PrintMemory(arr [10]byte) {
 	z01.PrintRune('\n')
 
 	for _, ch := range arr {
-		if ch >= 32 && ch <= 126 {
+		if ch > 32 && ch <= 126 {
 			z01.PrintRune(rune(ch))
 		} else {
 			z01.PrintRune('.')
